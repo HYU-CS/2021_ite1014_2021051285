136d9e4256065f99c742ba98a981125cd53ef398
2021051285 <datmoyan@hanyang.ac.kr>
2021-04-16 14:09:32 +0900

    11th assignment update

--- 7-2/1/1.c
+++ 7-2/1/1.c
@@ -5,9 +5,9 @@ int main()
     float f1 = 1.23456789;
     float f2 = 0.0000123456789;
 
-    printf("%.10f %.10f\n", f1, f2);
-    printf("%e %e\n", f1, f2);
-    printf("%E %E\n", f1, f2);
-    printf("%g %g\n", f1, f2);
-    printf("%G %G\n", f1, f2);
+    printf("1) %.10f %.10f\n", f1, f2);
+    printf("2) %e %e\n", f1, f2);
+    printf("3) %E %E\n", f1, f2);
+    printf("4) %g %g\n", f1, f2);
+    printf("5) %G %G\n", f1, f2);
 }
