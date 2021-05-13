062326bd24e5ea543c852fc623fb33d998268f39
2021051285 <datmoyan@hanyang.ac.kr>
2021-05-13 15:09:32 +0900

    11-1 update

--- 11-1/1/1.c
+++ 11-1/1/1.c
@@ -13,8 +13,8 @@ int main(void)
 
     printf("str1: %s\n", str1);
     printf("str2: %s\n", str2);
-    printf("length of str1: %lu\n", strlen(str1));
-    printf("length of str2: %lu\n", strlen(str2));
+    printf("length of str1: %ld\n", strlen(str1));
+    printf("length of str2: %ld\n", strlen(str2));
     printf("str1+str2: %s\n", joined_str);
 
     if(strcmp(str1, str2) == 0)
