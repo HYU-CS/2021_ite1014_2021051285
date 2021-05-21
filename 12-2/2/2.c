19d393b3f537c7e1068c3b333979abfc9f3b41ac
2021051285 <datmoyan@hanyang.ac.kr>
2021-05-21 10:52:34 +0900

    12-2 upload

---
+++ 12-2/2/2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+typedef struct
+{
+    char name[7];
+    int score;
+} Person;
+
+void printScoreStarts(const Person* persons, int len);
+
+int main(void)
+{
+    Person person_arr[3];
+
+    for(int i = 0; i < 3; i++)
+    {
+        scanf("%s%d", person_arr[i].name, &person_arr[i].score);
+    }
+
+    printScoreStarts(person_arr, 3);
+
+    return 0;
+}
+
+void printScoreStarts(const Person* persons, int len)
+{
+    for(int i = 0; i < len; i++)
+    {
+        printf("%-7s", (persons + i)->name);
+
+        for(int j = 0; j < (persons + i)->score / 5; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+}
